2c9fb852d50c64a33ad79dcc4a789684a195a775
siddharth_mantri <siddharthmantri1@gmail.com>
2015-12-07 06:55:30 -0500

    Fixed everything

--- app/nodetest.js
+++ app/nodetest.js
@@ -4,8 +4,7 @@ var mongodb = require('mongodb'),
 MongoClient = mongodb.MongoClient;
 var assert = require('assert');  
 var util=require('util');
-var url = 'mongodb://bhavin:pass123@ds059694.mongolab.com:59694/npm_db';
-//var url = 'mongodb://npwm_admin:pass123@ds061984.mongolab.com:61984/heroku_4j8g2kcv'
+var url = 'mongodb://npwm_admin:pass123@ds061984.mongolab.com:61984/heroku_4j8g2kcv'
 var app = express();  
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,24 +13,13 @@ var db;
 
 MongoClient.connect(url, function(err, database){
 	db = database;
-	db.collection("Restaurants",{ }, function(err,result){
+	db.collection("restaurant",{ }, function(err,result){
 		if(err!=null){
 			console.dir("Collection not found");
 		} else{console.dir("connected to Restaurants");}
 	});
-
-
-	/*	db.collection('Restaurants').createIndex(
-		{
-		 name:"text"
-		},function(err,indexname){
-			console.dir("created index");
-			assert.equal(err,null);
-		});*/
-
 app.listen(3000);
 
-/*http.createServer(app).listen(app.get('port'));*/
 });
 
 
@@ -47,14 +35,17 @@ app.get("/add", function(req,res){
 
 
 app.post("/restaurant/:restaurant_id", function(req, res) { 
-	restaurant_id = parseInt(req.params.restaurant_id);
-	commentArray=[req.body]
-	addComment = db.collection('Restaurants').update({"id": restaurant_id}, {$addToSet: {commentArray: req.body}
+	restaurant_id = req.params.restaurant_id;
+	commentArray=[req.body.comment]
+	addComment = db.collection('restaurant').update({"restaurant_id": restaurant_id}, {"$addToSet": {"review": {'$each': commentArray}}
   }, function(err, result) {
     if (err == null) {
-    	db.collection('Restaurants').find({"id": restaurant_id}).toArray(function(err, response){
+    	db.collection('restaurant').find({"restaurant_id": restaurant_id}).toArray(function(err, response){
  		res.setHeader('Content-Type', 'application/json');
-    	res.send(JSON.stringify(response));
+    	res.send({
+	    	'response': response,
+	    	'success': true
+	    });
  	});
     } else {
       res.send({
@@ -68,8 +59,7 @@ app.post("/restaurant/:restaurant_id", function(req, res) {
 
 app.get('/restaurant/:restaurant_id', function(req,res){
 	restaurant_id= req.params.restaurant_id;
-	console.log(restaurant_id);
- 	db.collection('Restaurants').find({"restaurant_id": restaurant_id}).toArray(function(err, response){
+ 	db.collection('restaurant').find({"restaurant_id": restaurant_id}).toArray(function(err, response){
  		res.setHeader('Content-Type', 'application/json');
     res.send({
     	'response': response,
@@ -84,25 +74,9 @@ app.get('/restaurant/:restaurant_id', function(req,res){
 
 app.get('/search', function(req,res){
 	var query = req.query.q;
-	console.dir(query)
-	var reg = "\""+query+"\"";
-	console.dir(reg)
-	db.collection('Restaurants').find({
-		"$text":{
-			"$search":reg
-		}
-	},
-	{
-		name:1,
-		borough:1,
-		restaurant_id:1, // need all fields from the collection
-		
-			score:
-			{
-				$meta: "textScore"
-			}
-		
-	}).toArray(function(err,items){
+	var reg = ".*(?i)"+query+".*"
+	// var reg = "\""+query+"\"";
+	db.collection('restaurant').find({name: {$regex: reg}}).toArray(function(err,items){
 		res.setHeader('Content-Type', 'application/json');
 		res.send({
 			'success': true,
@@ -110,17 +84,3 @@ app.get('/search', function(req,res){
 		});
 	})
 });
-
-
-function pagelist(items){ //using each item from the resulting array above to display the field values
-	result= "<html><body><ul>";
-	items.forEach(function(item){
-		itemstring = "<li>" + item.id + "<ul><li>" + item.score + 
-		"</li><li>" + item.fromUserName + "</li><li>" + item.text + 
-		"<p><b>+++++++++++END OF DOCUMENT++++++++++</ul></b></p>";
-		postbox = "<form method=\"post\"><textarea class=\"text\" cols=\"40\" rows =\"5\" name=\"newDocumentField\"></textarea><br/><input type=\"submit\" value=\"Add\" class=\"submitButton\">"
-		result = result + itemstring + postbox;
-	});
-	result = result + "</ul></body></html>";
-	return result;
-}
